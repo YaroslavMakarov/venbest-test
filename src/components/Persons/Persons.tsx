b0ba868c1faa5a38c9ac609b8cf1be94a7acc2b8
Yarik1993 <makarov1603@icloud.com>
2020-10-26 14:50:36 +0200

    persons modified

--- src/components/Persons/Persons.tsx
+++ src/components/Persons/Persons.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useMemo, useState} from "react";
 import { useSelector } from "react-redux";
 import {
-    ageFilterSelector, femaleFilterSelector,
+    ageFilterSelector,
+    femaleFilterSelector,
     lastNameFilterSelector,
     maleFilterSelector,
     nameFilterSelector,
